d0f648165d02ba493edf06dcfd5c4d183ef1afb9
Deivid Lourenço <deivid.soares@teevo.com.br>
2017-06-20 13:14:26 -0300

    Commit Test

--- Babel_Fish/Flask/app/client/interface/teste1/teste.py
+++ Babel_Fish/Flask/app/client/interface/teste1/teste.py
@@ -1,7 +1,6 @@
 
 from app import app #import padrão da instancia Flask
 
-
 @app.route('/') #Define a rota
 def teste():
-    return "Testando Estrutura de pastas e funcionamento Básico dentro do VS"
+    return "Testando Estrutura de pastas e funcionamento Básico dentro do VS asdadasd"
