9dd504e8041be96ab0a007a7ba6e87c08afa116a
Deivid Lourenço <deivid.soares@teevo.com.br>
2017-06-22 14:02:00 -0300

    funcionalidade_X_aprimorada(teste)

--- Babel_Fish/Flask/app/__init__.py
+++ Babel_Fish/Flask/app/__init__.py
@@ -5,9 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 from app.client.interface.basic_interface import teste #chamada de aplicação
-
+from flask import render_template # Basicamente transforma HTML em uma string
 
 
 @app.route('/algum_lugar/')
 def algum_lugar():  
-        return "Algum_Lugar Aqui"
+        return "----------------------"
+
+@app.route('/atualizar/')
+def atualizar():
+    return render_template('atualizar_interface.html')
--- Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
+++ Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
@@ -2,9 +2,11 @@
 from flask import render_template # Basicamente transforma HTML em uma string
 from app import app #import padrão da instancia Flask
 
+
 @app.route('/') # Define a rota
 def basic_interface():
-    return render_template('basic_interface.html') # Sem caminho necessário (Templates)
+    dv = [123245334234, 21353445656, 'TIRA FQ TUBO SMTH', 0, 2324354634, 0, 183218, 10, 'METALSA CA', '00/00/00', 'Descrição', 6565, 432546546, 1,443, 21, 32]
+    return render_template('basic_interface.html', dv = dv) # Sem caminho necessário (Templates)
 
 
 
