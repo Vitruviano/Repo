f3f1966d927a65875aeac784f284c5c373d02c49
Deivid Lourenço <deivid.soares@teevo.com.br>
2017-06-23 16:34:49 -0300

    Interface_update_23/06/17

--- Babel_Fish/Flask/run.py
+++ Babel_Fish/Flask/run.py
@@ -1,11 +1,11 @@
-#from flask_script import Manager
+from flask_script import Manager
 from app import app
 
-#manager = Manager(app)
+manager = Manager(app)
 
-#@manager.command
-#def hello():
-#    print ("hello")
+@manager.command
+def hello():
+    print ("hello")
 
 if __name__ == "__main__":
-	app.run()
+	manager.run()
