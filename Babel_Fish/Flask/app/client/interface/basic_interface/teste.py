266f17258dc6fb34773a5e0c2e81bebac7a2035b
Deivid Lourenço <deivid.soares@teevo.com.br>
2017-06-25 17:16:14 -0300

    Interface_update_25/06/17

--- Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
+++ Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
@@ -1,14 +1,36 @@
 
 from flask import render_template # Basicamente transforma HTML em uma string
 from app import app #import padrão da instancia Flask
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
+
+dv = [123245334234, 21353445656, 'TIRA FQ TUBO SMTH', 0, 2324354634, 0, 183218, 10, 'METALSA CA', '00/00/00', 'Descrição', 6565, 432546546, 1,443, 21, 'MTM 125 05']
+dv2 = [20280, 20279, 'F05', 'T25', 1421, 'Pronto', 'METALSA CA', 'TUBO RET S460MC 60 X 100 X 4,00 FQ', 'Stringse', 6860, 0, '00/00/00-13:34', 736542, 0, 20, '21:09:43', 'P']
+   
 
 
-@app.route('/') # Define a rota
+
+class Btnform(FlaskForm):
+    btn = SubmitField('Inicia Produção')
+
+
+
+@app.route('/', methods=['GET','POST']) # Define a rota
 def basic_interface():
-    dv = [123245334234, 21353445656, 'TIRA FQ TUBO SMTH', 0, 2324354634, 0, 183218, 10, 'METALSA CA', '00/00/00', 'Descrição', 6565, 432546546, 1,443, 21, 'MTM 125 05']
-    dv2 = [20280, 20279, 'F05', 'T25', 1421, 'Pronto', 'METALSA CA', 'TUBO RET S460MC 60 X 100 X 4,00 FQ', 'Stringse', 6860, 0, '00/00/00-13:34', 736542, 0, 20, '21:09:43', 'P']
-    return render_template('basic_interface.html', dv = dv, dv2 = dv2) # Sem caminho necessário (Templates)
+    global dv, dv2
 
+    form = Btnform()
+    if (form.btn.data == True):
+        teste(form.btn.data)
+   
+    return render_template('basic_interface.html', dv = dv, dv2 = dv2, form = form) # Sem caminho necessário (Templates)
+
+
+def teste(var):
+   
+    print(str(var))
+    
 
 
 
--- Babel_Fish/Flask/run.py
+++ Babel_Fish/Flask/run.py
@@ -2,6 +2,7 @@ from flask_script import Manager
 from app import app
 
 manager = Manager(app)
+app.secret_key = 'someverycomplexkey'
 
 @manager.command
 def hello():
