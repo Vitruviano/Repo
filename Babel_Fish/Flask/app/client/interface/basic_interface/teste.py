4a57a50e6ea95895f476695eee5fcd833a996352
Deivid Lourenço <deivid.soares@teevo.com.br>
2017-07-03 13:26:39 -0300

    Interface_update_03/07/17

--- Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
+++ Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
@@ -1,5 +1,5 @@
-
 from flask import render_template # Basicamente transforma HTML em uma string
+from flask import jsonify, request
 from app import app #import padrão da instancia Flask
 from flask_wtf import FlaskForm #Import necessãrio para utilização do método POST
 from wtforms import SubmitField #Import do tipo de POST que será feito
@@ -9,6 +9,26 @@ dv = [123245334234, 21353445656, 'TIRA FQ TUBO SMTH', 0, 2324354634, 0, 183218, 
 dv2 = [20280, 20279, 'F05', 'T25', 1421, 'Pronto', 'METALSA CA', 'TUBO RET S460MC 60 X 100 X 4,00 FQ', 'Stringse', 6860, 0, '00/00/00-13:34', 736542, 0, 20, '21:09:43', 'P'] #Variáveis teste
    
 
+class Pedidos():
+    def __init__(self, pedido1, pedido2, pedido3, pedido4):
+        self.pedido1 = pedido1
+        self.pedido2 = pedido2
+        self.pedido3 = pedido3
+        self.pedido4 = pedido4
+
+    def delete(self, pedido):
+        if (pedido == self.pedido1):
+            print("Pedido1")
+            self.pedido1 = 'Null'
+        if (pedido == self.pedido2):
+            print("Pedido2")
+            self.pedido2 = 'Null'
+        if (pedido == self.pedido3):
+            print("Pedido3")
+            self.pedido3 = 'Null'
+
+
+classe = Pedidos('123', '456', '789', '2231')
 
 
 class Btnform(FlaskForm): #Criação da classe de formulários que voltarão em forma de POST 
@@ -18,19 +38,43 @@ class Btnform(FlaskForm): #Criação da classe de formulários que voltarão em 
 
 @app.route('/', methods=['GET','POST']) #Roteamento 
 def basic_interface():
-    global dv, dv2
+    global dv, dv2, classe
 
     form = Btnform() #Criação de uma instância da classe
     if (form.btn.data == True): #Verifica se o botão de POST foi pressionado
         teste(form.btn.data) #Chama função fora do request handler
    
-    return render_template('basic_interface.html', dv = dv, dv2 = dv2, form = form) #Render de HTML localizado na pasta tramplates + Passagem de vars para HTML
+    return render_template('basic_interface.html', dv = dv, dv2 = dv2, form = form, classe = classe) #Render de HTML localizado na pasta tramplates + Passagem de vars para HTML
+
+
+@app.route('/background_process')
+def background_process():
+    global classe
+    classe.pedido1 = 'change1'
+    response = classe.pedido1
+    return jsonify(result = response)
+    
+
+
+@app.route('/background_process2')
+def background_process2():
+    global classe  
+    classe.pedido2 = 'chsange2'
+    response = classe.pedido2
+    return jsonify(result2 = response)
+
+
+@app.route('/background_process3')
+def background_process3():
+    global classe  
+    classe.pedido3 = 'change3'
+    response = classe.pedido3
+    return jsonify(result3 = response)
+
 
 
 def teste(var): #Criação da função que será chamada pelo POST do usuário 
    
     print(str(var))
     
-
-
-
+classe = Pedidos('123', '456', '789', '001')
