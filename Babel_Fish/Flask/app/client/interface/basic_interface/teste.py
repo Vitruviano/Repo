76c1918aeb6e4ae72bcb7d63bbb8355b5ec3ff7e
Deivid Lourenço <deivid.soares@teevo.com.br>
2017-07-11 20:50:08 -0300

    Run_update_11/07/17

--- Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
+++ Babel_Fish/Flask/app/client/interface/basic_interface/teste.py
@@ -2,7 +2,8 @@ from flask import render_template # Basicamente transforma HTML em uma string
 from flask import jsonify, request
 from app import app #import padrão da instancia Flask
 from flask_wtf import FlaskForm #Import necessãrio para utilização do método POST
-from wtforms import SubmitField #Import do tipo de POST que será feito
+from wtforms import SubmitField, StringField, PasswordField #Import do tipo de POST que será feito
+from wtforms.validators import DataRequired
 
 
 dv = [123245334234, 21353445656, 'TIRA FQ TUBO SMTH', 0, 2324354634, 0, 183218, 10, 'METALSA CA', '00/00/00', 'Descrição', 6565, 432546546, 1,443, 21, 'MTM 125 05'] #Variáveis teste
@@ -26,14 +27,21 @@ class Pedidos():
         if (pedido == self.pedido3):
             print("Pedido3")
             self.pedido3 = 'Null'
-
-
 classe = Pedidos('123', '456', '789', '2231')
 
 
 class Btnform(FlaskForm): #Criação da classe de formulários que voltarão em forma de POST 
     btn = SubmitField('Inicia Produção') #Indicação do tipo de campo do formulário
+    btn2 = SubmitField('Enviear')
+    username = StringField('Endereço', validators=[DataRequired])
+    password = PasswordField('Senha', validators=[DataRequired])
 
+@app.route('/login')
+def login():
+    form2 = Btnform()
+    if (form2.btn.data == True):
+        print('Legal')
+    return render_template('login.html', form = form2)
 
 
 @app.route('/', methods=['GET','POST']) #Roteamento 
